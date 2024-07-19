e167058a410575e5eaa7042295d52195397516f8
Richie Roy Jayme <58279554+Queztion@users.noreply.github.com>
2024-07-19 13:59:06 +0900

    Update wwan_issue_fix.sh
    Removed version checking,
    added grep checking for /usr/sbin/ModemManager

--- wwan_issue_fix.sh
+++ wwan_issue_fix.sh
@@ -10,6 +10,10 @@ RM520_check=$(lspci -d :1007)
 EM160R_check=$(lspci -d :100d)
 CURRENT_CONFIG=$(cat "$SERVICE_FILE")
 
+USR_SBIN_QUICKSUSPEND="/usr/sbin/ModemManager --help | grep test-quick-suspend-resume"
+USR_SBIN_LOWPOWER="/usr/sbin/ModemManager --help | grep test-low-power-suspend-resume"
+bool_suspend_fix_supported=false
+
 restart_mm_service=false
 function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
 
@@ -18,7 +22,19 @@ if [ -n "$Rplus_check" ] || [ -n "$FM350_check" ] || [ -n "$RM520_check" ] || [ 
 	first_line=${curmmver%%\n*}
 	curmmvernum=$(echo $first_line | cut -d " " -f2)
 	stand_ver="1.23.2"
-	if version_ge $curmmvernum $stand_ver; then
+
+
+	if grep -q 'test-quick-suspend-resume' <<< $USR_SBIN_QUICKSUSPEND; then
+		bool_suspend_fix_supported=true
+	fi
+
+	if grep -q 'test-low-power-suspend-resume' <<< $USR_SBIN_LOWPOWER; then
+		bool_suspend_fix_supported=true
+	fi
+
+	
+	if $bool_suspend_fix_supported; then
+		# echo $bool_suspend_fix_supported
 		if grep -q 'test-quick-suspend-resume' <<< "$CURRENT_CONFIG";then
 			echo "test-quick-suspend-resume parameter already exists"
 		else
@@ -39,7 +55,8 @@ if [ -n "$Rplus_check" ] || [ -n "$FM350_check" ] || [ -n "$RM520_check" ] || [ 
 			sudo systemctl restart ModemManager
 		fi
 	else
-		echo "Fix supports ModemManager version greater than 1.23.2 or later only."
+		#echo "Fix supports ModemManager version greater than 1.23.2 or later only."
+		echo "Please download ModemManager 1.23.2 or later as it contains patches for Suspend Issue Fix."
 	fi
 else
 	echo "Issue Fix is only applicable for Fibocom L860-GL-16/FM350 and Quectel EM160R-GL/RM520N-GL WWAN module."
